443556f4110f77b83b5cd8bdd993b6403013e2d0
napster <khaousnoureddine02@gmail.com>
2024-12-17 00:34:34 +0100

    changed mlp with kan in nhits block

--- src/models/nhits/nhits-kan.py
+++ src/models/nhits/nhits-kan.py
@@ -15,6 +15,288 @@ from functools import partial
 from ..components.common import RepeatVector
 from ...losses.utils import LossFunction
 
+class KANLinear(t.nn.Module):
+    def __init__(
+        self,
+        in_features,
+        out_features,
+        grid_size=5,
+        spline_order=3,
+        scale_noise=0.1,
+        scale_base=1.0,
+        scale_spline=1.0,
+        enable_standalone_scale_spline=True,
+        base_activation=t.nn.SiLU,
+        grid_eps=0.02,
+        grid_range=[-1, 1],
+    ):  
+        super(KANLinear, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.grid_size = grid_size
+        self.spline_order = spline_order
+
+        h = (grid_range[1] - grid_range[0]) / grid_size
+        grid = (
+            (
+                t.arange(-spline_order, grid_size + spline_order + 1) * h
+                + grid_range[0]
+            )
+            .expand(in_features, -1)
+            .contiguous()
+        )
+        self.register_buffer("grid", grid)
+
+        self.base_weight = t.nn.Parameter(t.Tensor(out_features, in_features))
+        self.spline_weight = t.nn.Parameter(
+            t.Tensor(out_features, in_features, grid_size + spline_order)
+        )
+        if enable_standalone_scale_spline:
+            self.spline_scaler = t.nn.Parameter(
+                t.Tensor(out_features, in_features)
+            )
+
+        self.scale_noise = scale_noise
+        self.scale_base = scale_base
+        self.scale_spline = scale_spline
+        self.enable_standalone_scale_spline = enable_standalone_scale_spline
+        self.base_activation = base_activation()
+        self.grid_eps = grid_eps
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        t.nn.init.kaiming_uniform_(self.base_weight, a=math.sqrt(5) * self.scale_base)
+        with t.no_grad():
+            noise = (
+                (
+                    t.rand(self.grid_size + 1, self.in_features, self.out_features)
+                    - 1 / 2
+                )
+                * self.scale_noise
+                / self.grid_size
+            )
+            self.spline_weight.data.copy_(
+                (self.scale_spline if not self.enable_standalone_scale_spline else 1.0)
+                * self.curve2coeff(
+                    self.grid.T[self.spline_order : -self.spline_order],
+                    noise,
+                )
+            )
+            if self.enable_standalone_scale_spline:
+                # t.nn.init.constant_(self.spline_scaler, self.scale_spline)
+                t.nn.init.kaiming_uniform_(self.spline_scaler, a=math.sqrt(5) * self.scale_spline)
+
+    def b_splines(self, x: t.Tensor):
+        """
+        Compute the B-spline bases for the given input tensor.
+
+        Args:
+            x (torch.Tensor): Input tensor of shape (batch_size, in_features).
+
+        Returns:
+            torch.Tensor: B-spline bases tensor of shape (batch_size, in_features, grid_size + spline_order).
+        """
+        assert x.dim() == 2 and x.size(1) == self.in_features
+
+        grid: t.Tensor = (
+            self.grid
+        )  # (in_features, grid_size + 2 * spline_order + 1)
+        x = x.unsqueeze(-1)
+        bases = ((x >= grid[:, :-1]) & (x < grid[:, 1:])).to(x.dtype)
+        for k in range(1, self.spline_order + 1):
+            bases = (
+                (x - grid[:, : -(k + 1)])
+                / (grid[:, k:-1] - grid[:, : -(k + 1)])
+                * bases[:, :, :-1]
+            ) + (
+                (grid[:, k + 1 :] - x)
+                / (grid[:, k + 1 :] - grid[:, 1:(-k)])
+                * bases[:, :, 1:]
+            )
+
+        assert bases.size() == (
+            x.size(0),
+            self.in_features,
+            self.grid_size + self.spline_order,
+        )
+        return bases.contiguous()
+
+    def curve2coeff(self, x: t.Tensor, y: t.Tensor):
+        """
+        Compute the coefficients of the curve that interpolates the given points.
+
+        Args:
+            x (torch.Tensor): Input tensor of shape (batch_size, in_features).
+            y (torch.Tensor): Output tensor of shape (batch_size, in_features, out_features).
+
+        Returns:
+            torch.Tensor: Coefficients tensor of shape (out_features, in_features, grid_size + spline_order).
+        """
+        assert x.dim() == 2 and x.size(1) == self.in_features
+        assert y.size() == (x.size(0), self.in_features, self.out_features)
+
+        A = self.b_splines(x).transpose(
+            0, 1
+        )  # (in_features, batch_size, grid_size + spline_order)
+        B = y.transpose(0, 1)  # (in_features, batch_size, out_features)
+        solution = t.linalg.lstsq(
+            A, B
+        ).solution  # (in_features, grid_size + spline_order, out_features)
+        result = solution.permute(
+            2, 0, 1
+        )  # (out_features, in_features, grid_size + spline_order)
+
+        assert result.size() == (
+            self.out_features,
+            self.in_features,
+            self.grid_size + self.spline_order,
+        )
+        return result.contiguous()
+
+    @property
+    def scaled_spline_weight(self):
+        return self.spline_weight * (
+            self.spline_scaler.unsqueeze(-1)
+            if self.enable_standalone_scale_spline
+            else 1.0
+        )
+
+    def forward(self, x: t.Tensor):
+        assert x.size(-1) == self.in_features
+        original_shape = x.shape
+        x = x.reshape(-1, self.in_features)
+
+        base_output = F.linear(self.base_activation(x), self.base_weight)
+        spline_output = F.linear(
+            self.b_splines(x).view(x.size(0), -1),
+            self.scaled_spline_weight.view(self.out_features, -1),
+        )
+        output = base_output + spline_output
+        
+        output = output.reshape(*original_shape[:-1], self.out_features)
+        return output
+
+    @t.no_grad()
+    def update_grid(self, x: t.Tensor, margin=0.01):
+        assert x.dim() == 2 and x.size(1) == self.in_features
+        batch = x.size(0)
+
+        splines = self.b_splines(x)  # (batch, in, coeff)
+        splines = splines.permute(1, 0, 2)  # (in, batch, coeff)
+        orig_coeff = self.scaled_spline_weight  # (out, in, coeff)
+        orig_coeff = orig_coeff.permute(1, 2, 0)  # (in, coeff, out)
+        unreduced_spline_output = t.bmm(splines, orig_coeff)  # (in, batch, out)
+        unreduced_spline_output = unreduced_spline_output.permute(
+            1, 0, 2
+        )  # (batch, in, out)
+
+        # sort each channel individually to collect data distribution
+        x_sorted = t.sort(x, dim=0)[0]
+        grid_adaptive = x_sorted[
+            t.linspace(
+                0, batch - 1, self.grid_size + 1, dtype=t.int64, device=x.device
+            )
+        ]
+
+        uniform_step = (x_sorted[-1] - x_sorted[0] + 2 * margin) / self.grid_size
+        grid_uniform = (
+            t.arange(
+                self.grid_size + 1, dtype=t.float32, device=x.device
+            ).unsqueeze(1)
+            * uniform_step
+            + x_sorted[0]
+            - margin
+        )
+
+        grid = self.grid_eps * grid_uniform + (1 - self.grid_eps) * grid_adaptive
+        grid = t.concatenate(
+            [
+                grid[:1]
+                - uniform_step
+                * t.arange(self.spline_order, 0, -1, device=x.device).unsqueeze(1),
+                grid,
+                grid[-1:]
+                + uniform_step
+                * t.arange(1, self.spline_order + 1, device=x.device).unsqueeze(1),
+            ],
+            dim=0,
+        )
+
+        self.grid.copy_(grid.T)
+        self.spline_weight.data.copy_(self.curve2coeff(x, unreduced_spline_output))
+
+    def regularization_loss(self, regularize_activation=1.0, regularize_entropy=1.0):
+        """
+        Compute the regularization loss.
+
+        This is a dumb simulation of the original L1 regularization as stated in the
+        paper, since the original one requires computing absolutes and entropy from the
+        expanded (batch, in_features, out_features) intermediate tensor, which is hidden
+        behind the F.linear function if we want an memory efficient implementation.
+
+        The L1 regularization is now computed as mean absolute value of the spline
+        weights. The authors implementation also includes this term in addition to the
+        sample-based regularization.
+        """
+        l1_fake = self.spline_weight.abs().mean(-1)
+        regularization_loss_activation = l1_fake.sum()
+        p = l1_fake / regularization_loss_activation
+        regularization_loss_entropy = -t.sum(p * p.log())
+        return (
+            regularize_activation * regularization_loss_activation
+            + regularize_entropy * regularization_loss_entropy
+        )
+
+
+class KAN(t.nn.Module):
+    def __init__(
+        self,
+        layers_hidden,
+        grid_size=5,
+        spline_order=3,
+        scale_noise=0.1,
+        scale_base=1.0,
+        scale_spline=1.0,
+        base_activation=t.nn.SiLU,
+        grid_eps=0.02,
+        grid_range=[-1, 1],
+    ):
+        super(KAN, self).__init__()
+        self.grid_size = grid_size
+        self.spline_order = spline_order
+
+        self.layers = t.nn.ModuleList()
+        for in_features, out_features in zip(layers_hidden, layers_hidden[1:]):
+            self.layers.append(
+                KANLinear(
+                    in_features,
+                    out_features,
+                    grid_size=grid_size,
+                    spline_order=spline_order,
+                    scale_noise=scale_noise,
+                    scale_base=scale_base,
+                    scale_spline=scale_spline,
+                    base_activation=base_activation,
+                    grid_eps=grid_eps,
+                    grid_range=grid_range,
+                )
+            )
+
+    def forward(self, x: t.Tensor, update_grid=False):
+        for layer in self.layers:
+            if update_grid:
+                layer.update_grid(x)
+            x = layer(x)
+        return x
+
+    def regularization_loss(self, regularize_activation=1.0, regularize_entropy=1.0):
+        return sum(
+            layer.regularization_loss(regularize_activation, regularize_entropy)
+            for layer in self.layers
+        )
+
+
 # Cell
 class _StaticFeaturesEncoder(nn.Module):
     def __init__(self, in_features, out_features):
@@ -103,88 +385,77 @@ ACTIVATIONS = ['ReLU',
                'PReLU',
                'Sigmoid']
 
+
+# nhits with KAN :
 class _NHITSBlock(nn.Module):
     """
-    N-HiTS block which takes a basis function as an argument.
+    N-HiTS block using KAN for the projection instead of MLP.
     """
     def __init__(self, n_time_in: int, n_time_out: int, n_x: int,
                  n_s: int, n_s_hidden: int, n_theta: int, n_theta_hidden: list,
                  n_pool_kernel_size: int, pooling_mode: str, basis: nn.Module,
-                 n_layers: int,  batch_normalization: bool, dropout_prob: float, activation: str):
-        """
-        """
+                 n_layers: int, batch_normalization: bool, dropout_prob: float, activation: str):
         super().__init__()
 
-        assert (pooling_mode in ['max','average'])
+        assert pooling_mode in ['max', 'average']
 
-        n_time_in_pooled = int(np.ceil(n_time_in/n_pool_kernel_size))
-
+        # Compute input dimension to KAN
+        n_time_in_pooled = int(np.ceil(n_time_in / n_pool_kernel_size))
         if n_s == 0:
             n_s_hidden = 0
-        n_theta_hidden = [n_time_in_pooled + (n_time_in+n_time_out)*n_x + n_s_hidden] + n_theta_hidden
+        n_input_features = n_time_in_pooled + (n_time_in + n_time_out) * n_x + n_s_hidden
 
-        self.n_time_in = n_time_in
-        self.n_time_out = n_time_out
-        self.n_s = n_s
-        self.n_s_hidden = n_s_hidden
-        self.n_x = n_x
-        self.n_pool_kernel_size = n_pool_kernel_size
-        self.batch_normalization = batch_normalization
-        self.dropout_prob = dropout_prob
+        # KAN remplacera le MLP ici
+        self.kan = KAN(
+            layers_hidden=[n_input_features] + n_theta_hidden + [n_theta],  # Liste des dimensions des couches
+            grid_size=5,  # Taille de la grille des splines
+            spline_order=3,  # Ordre des splines
+            scale_noise=0.1,
+            scale_base=1.0,
+            scale_spline=1.0,
+            base_activation=t.nn.SiLU,  # Fonction d'activation
+            grid_eps=0.02,
+            grid_range=[-1, 1]
+        )
 
-        assert activation in ACTIVATIONS, f'{activation} is not in {ACTIVATIONS}'
-        activ = getattr(nn, activation)()
-
-        if pooling_mode == 'max':
-            self.pooling_layer = nn.MaxPool1d(kernel_size=self.n_pool_kernel_size,
-                                              stride=self.n_pool_kernel_size, ceil_mode=True)
-        elif pooling_mode == 'average':
-            self.pooling_layer = nn.AvgPool1d(kernel_size=self.n_pool_kernel_size,
-                                              stride=self.n_pool_kernel_size, ceil_mode=True)
-
-        hidden_layers = []
-        for i in range(n_layers):
-            hidden_layers.append(nn.Linear(in_features=n_theta_hidden[i], out_features=n_theta_hidden[i+1]))
-            hidden_layers.append(activ)
-
-            if self.batch_normalization:
-                hidden_layers.append(nn.BatchNorm1d(num_features=n_theta_hidden[i+1]))
-
-            if self.dropout_prob>0:
-                hidden_layers.append(nn.Dropout(p=self.dropout_prob))
-
-        output_layer = [nn.Linear(in_features=n_theta_hidden[-1], out_features=n_theta)]
-        layers = hidden_layers + output_layer
-
-        # n_s is computed with data, n_s_hidden is provided by user, if 0 no statics are used
-        if (self.n_s > 0) and (self.n_s_hidden > 0):
+        # Static encoder pour les variables statiques
+        if (n_s > 0) and (n_s_hidden > 0):
             self.static_encoder = _StaticFeaturesEncoder(in_features=n_s, out_features=n_s_hidden)
-        self.layers = nn.Sequential(*layers)
+
+        # Pooling Layer
+        if pooling_mode == 'max':
+            self.pooling_layer = nn.MaxPool1d(kernel_size=n_pool_kernel_size, stride=n_pool_kernel_size, ceil_mode=True)
+        elif pooling_mode == 'average':
+            self.pooling_layer = nn.AvgPool1d(kernel_size=n_pool_kernel_size, stride=n_pool_kernel_size, ceil_mode=True)
+
         self.basis = basis
 
     def forward(self, insample_y: t.Tensor, insample_x_t: t.Tensor,
                 outsample_x_t: t.Tensor, x_s: t.Tensor) -> Tuple[t.Tensor, t.Tensor]:
 
+        # Appliquer le pooling
         insample_y = insample_y.unsqueeze(1)
-        # Pooling layer to downsample input
-        insample_y = self.pooling_layer(insample_y)
-        insample_y = insample_y.squeeze(1)
+        insample_y = self.pooling_layer(insample_y).squeeze(1)
 
+        # Concatenation des features temporelles et exogènes
         batch_size = len(insample_y)
         if self.n_x > 0:
-            insample_y = t.cat(( insample_y, insample_x_t.reshape(batch_size, -1) ), 1)
-            insample_y = t.cat(( insample_y, outsample_x_t.reshape(batch_size, -1) ), 1)
+            insample_y = t.cat((insample_y, insample_x_t.reshape(batch_size, -1)), 1)
+            insample_y = t.cat((insample_y, outsample_x_t.reshape(batch_size, -1)), 1)
 
-        # Static exogenous
-        if (self.n_s > 0) and (self.n_s_hidden > 0):
+        # Encoder les variables statiques si disponibles
+        if (self.n_s > 0) and hasattr(self, 'static_encoder'):
             x_s = self.static_encoder(x_s)
             insample_y = t.cat((insample_y, x_s), 1)
 
-        # Compute local projection weights and projection
-        theta = self.layers(insample_y)
+        # Projection avec KAN
+        theta = self.kan(insample_y)
+
+        # Calculer le backcast et le forecast via la base
         backcast, forecast = self.basis(theta, insample_x_t, outsample_x_t)
 
         return backcast, forecast
+
 
 # Cell
 class _NHITS(nn.Module):
